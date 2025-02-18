7cacc0a97abee2f3600311c2b5d1d3b2dff5be83
shimfff <shimseawon1510@gmail.com>
2025-02-18 17:53:30 +0900

    :bug: fix: 버그 수정

--- src/main/java/gaji/service/domain/room/web/dto/response/ToggleAssignmentResponseDto.java
+++ src/main/java/gaji/service/domain/room/web/dto/response/ToggleAssignmentResponseDto.java
@@ -1,12 +1,16 @@
 package gaji.service.domain.room.web.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
+@Getter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ToggleAssignmentResponseDto {
     private List<ToggleAssignment> assignments;
 
